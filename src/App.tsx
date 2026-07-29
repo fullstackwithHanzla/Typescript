766e4f325d937934f1736f0d9092bff2f66a5397
Hanzla <fullstackwithhanzla@gmail.com>
2026-07-29 18:36:59 +0500

    TypeScript Starting Point

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+
+const App = () => {
+  return (
+    <div>
+        <h1 className="text-3xl font-medium w-full my-5 text-center">
+          TypeScript Series
+        </h1>
+        
+    </div>
+  )
+}
+
+export default App
