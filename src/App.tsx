5a6607d8636c51f155b3de1af705d2c88d20cc10
Hanzla <fullstackwithhanzla@gmail.com>
2026-07-29 18:42:22 +0500

    TypeScript Started

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ const App = () => {
   return (
     <div>
         <h1 className="text-3xl font-medium w-full my-5 text-center">
-          TypeScript Series
+          TypeScript Series 
         </h1>
         
     </div>
